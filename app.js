631f0548c2daca83eb28d7ba0e536abf2fcf9d2e
Aysegul Yonet <aysegulyonet@gmail.com>
2015-01-09 23:50:42 -0800

    Comment input added.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var app = angular.module('catstagram', []);
 app.controller('CatController', ['$scope', '$http', function($scope, $http){
 
+
+	$scope.comments = [];
+	$scope.comment = {};
 	$http.get('data.json')
 		.success(function(data, status, headers, config) {
 			$scope.cats = data.cats;
 		});
+
 	$scope.like = function(index) {
 		$scope.cats[index].likes ++;
 	}
@@ -13,5 +17,11 @@ app.controller('CatController', ['$scope', '$http', function($scope, $http){
 		newCat.likes = 1;
 		$scope.cats.unshift(newCat);
 		$scope.newCat = {};
+	}
+
+	$scope.submitComment = function() {
+		$scope.comments.unshift($scope.comment.text);
+		console.log('$scope.comments', $scope.comments)
+		$scope.comment.text = "";
 	}
 }]);
