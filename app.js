bcb50e6d4fc4fb2c1b7ff3b5a42410141711889b
Aysegul Yonet <aysegulyonet@gmail.com>
2015-01-09 20:14:52 -0800

    Sass files added,

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ app.controller('CatController', ['$scope', '$http', function($scope, $http){
 	$http.get('data.json')
 		.success(function(data, status, headers, config) {
 			$scope.cats = data.cats;
-			console.log('$scope.cats', $scope.cats)
 		});
 	$scope.like = function(index) {
 		$scope.cats[index].likes ++;
