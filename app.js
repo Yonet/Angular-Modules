1ba0abf53115d6d5ffa54fba98f92cee6fd3e0d8
Yonet <aysegul.yonet@solutionset.com>
2015-01-19 23:55:11 -0800

    Number format filter added.

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-var app = angular.module('catstagram', []);
-app.controller('CatController', ['$scope', '$http', function($scope, $http){
+var app = angular.module('ayModules', []);
 
-	$http.get('data.json')
-		.success(function(data, status, headers, config) {
-			$scope.cats = data.cats;
-			console.log('$scope.cats', $scope.cats)
-		});
-	$scope.like = function(index) {
-		$scope.cats[index].likes ++;
-	}
-
-	$scope.addCat = function(newCat) {
-		newCat.likes = 1;
-		$scope.cats.unshift(newCat);
-		$scope.newCat = {};
-	}
-}]);
+app.filter('numberFormat', [function () {
+		return function (number) {
+			if (number !== undefined) {
+				var abs = Math.abs(number);
+				if (abs >= Math.pow(10,12)) {//trillion
+					number =  (number / Math.pow(10, 12)).toFixed(1)+ 't';
+				} else if (abs < Math.pow(10,12) && abs >= Math.pow(10, 9)) {//billion
+					number =  (number / Math.pow(10, 9)).toFixed(1)+ 'b';
+				} else if (abs < Math.pow(10, 9) && abs >= Math.pow(10, 6)) {//million
+					number =  (number / Math.pow(10, 6)).toFixed(1)+ 'm';
+				} else if (abs < Math.pow(10, 6) && abs >= Math.pow(10, 3)) {//thousand
+					number =  (number / Math.pow(10,3)).toFixed(1)+ 'k';
+				}
+			}
+			return number;
+		};
+	}])
